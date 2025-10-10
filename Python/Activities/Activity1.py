c7b4f693e5668a374aab6881bad6bd180e241f5e
SravaniVallipalli <97835563+SravaniVallipalli@users.noreply.github.com>
2025-10-10 10:48:38 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,8 @@
+name=input("enter you name: ")
+age=int(input("enter your age :"))
+
+#calculate age when he turns 100
+year_100=(100-age)+2025
+#print the year they turn 100
+print(f"you will turn in the year {year_100}")
+#here we are concatenating the string year_100 with formatsr
